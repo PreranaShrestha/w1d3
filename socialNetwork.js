a65ddb852c9c0fc0bd43aba3bf8f22e81ff56b5e
PreranaShrestha <prerana.sh@gmail.com>
2018-01-19 06:17:15 +0000

    Add follower and follows above 30 in the function

--- socialNetwork.js
+++ socialNetwork.js
@@ -73,7 +73,57 @@ var mostFollower = function() {
   return Object.keys(followerList).filter(function(follow){
     return followerList[follow] === max;
   });
-return followerList;
+  return followerList;
 };
 console.log(mostFollower());
+
+////Identify who follows the most people above 30
+var followMostAbove30 = function() {
+  var followList = {};
+  for(var id in data) {
+    var follows = data[id].follows;
+    followList[id] = follows.length;
+  }
+  var vals = Object.keys(followList).map(function(key) {
+    return followList[key];
+  });
+  var max = Math.max(...vals);
+  var maxFollows = Object.keys(followList).filter(function(follow){
+    return followList[follow] === max;
+  });
+  return maxFollows.filter(function(id) {
+    return data[id].age > 30;
+
+  });
+
+};
+console.log(followMostAbove30());
+
+//who has the most follower above 30
+var mostFollowerAbove30 = function() {
+  var followerList = [];
+  for(var id in data) {
+    followerList[id] = 0;
+    for(var id1 in data) {
+      var follows = data[id1].follows;
+      follows.forEach(function(follow) {
+        if(id === follow) {
+          followerList[id] += 1;
+        }
+      });
+    }
+  }
+  var vals = Object.keys(followerList).map(function(key) {
+    return followerList[key];
+  });
+
+  var max = Math.max(...vals);
+  var maxFollowers = Object.keys(followerList).filter(function(follow){
+    return followerList[follow] === max;
+  });
+  return maxFollowers.filter(function(id) {
+  return data[id].age > 30;
+});
+};
+console.log(mostFollowerAbove30());
 
